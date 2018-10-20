9090ef8d8010478152ddbf1cfd5a671196bc2afc
Ash Hipgrave <AshHipgrave@users.noreply.github.com>
2018-10-20 17:34:41 +0100

    Began adding math functions to make working with Vectors easier

---
+++ src/pbrtlib/MathUtils.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Vector2.h";
+#include "Vector3.h";
+
+typedef Vector2<int> Vector2i;
+typedef Vector2<float> Vector2f;
+
+typedef Vector3<int> Vector3i;
+typedef Vector3<float> Vector3f;
+
+template <typename T> Vector3<T> inline Vector3<T> 
+operator*(T value, const Vector3<T>& vector)
+{
+	return vector * value;
+}
+
+template <typename T> Vector3<T> 
+Abs(const Vector3<T>& vector)
+{
+	return Vector3<T>(std::abs(vector.x), std::abs(vector.y), std::abs(vector.z));
+}
+
+template <typename T> inline T 
+Dot(const Vector3<T>& vector, const Vector3<T> otherVector)
+{
+	return (vector.x * otherVector.x) + (vector.y * otherVector.y) + (vector.z * otherVector.z);
+}
+
+template <typename T> inline T 
+AbsDot(const Vector3<T>& vector, const Vector3<T>& otherVector)
+{
+	return std::abs(Dot(vector, otherVector));
+}
+
